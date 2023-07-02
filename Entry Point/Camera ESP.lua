af20c4be47c8d2d109f6c9592f48e27e28b558e9
Irish <97372099+IrishBaker@users.noreply.github.com>
2023-07-02 00:14:48 -0700

    Update Camera ESP.lua

--- Entry Point/Camera ESP.lua
+++ Entry Point/Camera ESP.lua
@@ -1,1 +1,6 @@
--- Nothing here yet.
+local CamFolder = game:GetService("Workspace").Level.Glass
+for _,cam in pairs(CamFolder:GetDescendants()) do
+	if cam.Name =="Source" and cam:IsA("Part") then
+		local hl = Instance.new("Highlight", cam.Parent)
+	end
+end
