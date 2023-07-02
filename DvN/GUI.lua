a5ee97d82e903e35e24d013e92dacf2289618ac9
Irish <97372099+IrishBaker@users.noreply.github.com>
2023-07-02 05:51:43 -0700

    Yeah this is just a convenient gui for my scripts

---
+++ DvN/GUI.lua
@@ -0,0 +1,40 @@
+local InfiniteJumpEnabled = true
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("DvN", "Synapse")
+local Tab = Window:NewTab("Main")
+local Section = Tab:NewSection(" ")
+Section:NewButton("Inf Ammo", "", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/IrishBaker/REWORK/main/DvN/Inf%20Ammo.lua", true))()
+end)
+Section:NewButton("Damage Multiplier [ x6 ]", "", function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/IrishBaker/REWORK/main/DvN/Damage%20Multiplier.lua", true))()
+end)
+Section:NewButton("Hitbox Expander", "ButtonInfo", function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/IrishBaker/REWORK/main/DvN/Hitbox%20Extender.lua", true))()
+end)
+local Section = Tab:NewSection("Not finished")
+Section:NewButton("Auto Reload", "ButtonInfo", function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/IrishBaker/REWORK/main/DvN/Auto%20Reload.lua", true))()
+end)
+local Tab = Window:NewTab("Misc")
+local Sections = Tab:NewSection("random things")
+Sections:NewToggle("Day/Night", "", function(state)
+    if state then
+        game:GetService("Lighting").TimeOfDay = 12
+    else
+        game:GetService("Lighting").TimeOfDay = 24
+    end
+end)
+Sections:NewButton("No Fog", "", function()
+	game:GetService("Lighting").FogEnd = 9e9
+end)
+Sections:NewSlider("Walkspeed", "", 200, 0, function(s)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+Sections:NewButton("No Fog", "", function()
+	game:GetService("UserInputService").JumpRequest:connect(function()
+		if InfiniteJumpEnabled then
+			game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
+		end
+	end)
+end)
