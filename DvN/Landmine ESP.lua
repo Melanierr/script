715e9eecc07133b7f0dd6ec5172da66d7e40e4f1
Irish <97372099+IrishBaker@users.noreply.github.com>
2023-07-02 23:01:59 -0700

    Update Landmine ESP.lua

--- DvN/Landmine ESP.lua
+++ DvN/Landmine ESP.lua
@@ -2,11 +2,12 @@ function createESP(parent, name)
   if name == "Landmine" then
     local hl = Instance.new("Highlight")
     hl.Parent = parent
+  else return
   end
 end
 while true do
 game.Workspace.ChildAdded:Connect(function(obj)
       createESP(obj, obj.Name)
 end)
-wait()
+task.wait()
 end
