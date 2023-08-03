2cfed1a54ccd5a419efd661c9ab085b956ca5742
Irish <97372099+IrishBaker@users.noreply.github.com>
2023-08-03 04:08:03 -0700

    yeah fixed

--- DvN/Hitbox Extender.lua
+++ DvN/Hitbox Extender.lua
@@ -5,21 +5,25 @@ end
 local enemyNames = game:GetService("ReplicatedStorage").Units.Noobs:GetChildren()
 local mapFolder = game:GetService("Workspace").Map
 
-local function PutOnExtend(instance)
+local function PutOnExtend(i)
     for _, x in ipairs(enemyNames) do
-        if tostring(x) == instance.Name and instance.Head ~= nil and instance.Name ~= "APU" and instance.Name ~="Ranger" and instance.Name ~="Tank" and instance.Name ~="Platform" and instance ~= nil then
-            instance.Head.Transparency = 0.6
-            instance.Head.Size = Vector3.new(4.5, 4.5, 4.5)
+        if tostring(x) == instance.Name then
+            i:WaitForChild("Head").Transparency = 0.6
+            i:WaitForChild("Head").Size = Vector3.new(4.5, 4.5, 4.5)
         end
     end
 end
 
 for _, v in ipairs(workspace:GetChildren()) do
+   if v.Name ~= "Ranger" or v.Name ~= "APU" and v.Name ~= "Platform" and v.Name ~= "Tank" then
     task.wait()
     PutOnExtend(v)
+   end
 end
 
 workspace.ChildAdded:Connect(function(v)
+      if v.Name ~= "Ranger" or v.Name ~= "APU" and v.Name ~= "Platform" and v.Name ~= "Tank" then
     task.wait()
     PutOnExtend(v)
+      end
 end)
