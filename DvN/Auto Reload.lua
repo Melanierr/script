62592d934015c6a5c389f8102b1c6f44d153dcc5
Irish <97372099+IrishBaker@users.noreply.github.com>
2023-08-09 06:52:01 -0700

    removed due to bugs.

--- DvN/Auto Reload.lua
+++ DvN/Auto Reload.lua
@@ -1,16 +1,1 @@
-while true do
-  spawn(function()
-    for _,remote in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
-      if remote.Name =='VerifyReload' then
-        local wp = remote.Parent
-      end
-    end
-  end)
-wait(2)
-end
-while true do
-  spawn(function()
-      wp.VerifyReload:FireServer()
-      wait(.7)
-  end)
-end
+
