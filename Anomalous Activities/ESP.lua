01ef3f96f8f3b2a659b2fff5f4e7fa5e33614e29
Melanie <97372099+Melanierr@users.noreply.github.com>
2024-06-17 23:32:31 +0700

    Added a invisible check so you can fully see the anomly

--- Anomalous Activities/ESP.lua
+++ Anomalous Activities/ESP.lua
@@ -1,13 +1,13 @@
-spawn(function()
-    pcall(function()
-        for i,v in pairs(game.Workspace.mainGame["active_anomaly"]:GetChildren()) do
-            chams = Instance.new("Highlight", v)
-        end
-        game.Workspace.mainGame["active_anomaly"].ChildAdded:Connect(function(ins)
-            print("An instance has been added")
-            esp = Instance.new("Highlight", ins)
-            wait()
+game:GetService('RunService').RenderStepped:Connect(function()
+    for _,invis in ipairs(game.Workspace.mainGame["active_anomaly"]:GetDescendants()) do
+        pcall(function()
+            if invis.Transparency == 1 then
+                invis.Transparency = 0
+            end
         end)
-        print("succesfully")
-    end)
+    end
+    for _,obj in pairs(game.Workspace.mainGame["active_anomaly"]:GetChildren()) do
+        chams = Instance.new("Highlight", obj)
+    end
+    wait(10)
 end)
