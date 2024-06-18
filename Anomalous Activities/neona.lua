d6f82c9263a4f063f073a763d0b3166170af33ca
Melanie <97372099+Melanierr@users.noreply.github.com>
2024-06-18 13:36:48 +0700

    Update neona.lua

--- Anomalous Activities/neona.lua
+++ Anomalous Activities/neona.lua
@@ -1,7 +1,1 @@
-game:GetService('RunService').RenderStepped:Connect(function()
-    for _,part in ipairs(game.Workspace.mainGame["active_anomaly"]:GetDescendants()) do
-        pcall(function()
-            part.Material = "Neon"
-        end)
-    end
-end)
+
