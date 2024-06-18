9571c4240e0bfc8fd7a5dd2a090733a02f065222
Melanie <97372099+Melanierr@users.noreply.github.com>
2024-06-18 08:22:17 +0700

    Update ESP.lua

--- Anomalous Activities/ESP.lua
+++ Anomalous Activities/ESP.lua
@@ -1,13 +1,10 @@
 game:GetService('RunService').RenderStepped:Connect(function()
-    for _,invis in ipairs(game.Workspace.mainGame["active_anomaly"]:GetDescendants()) do
+    for _,obj in pairs(game.Workspace.mainGame["active_anomaly"]:GetChildren()) do
         pcall(function()
-            if invis.Transparency == 1 then
-                invis.Transparency = 0
+            if obj:FindFirstChildWhichIsA("Highlight") == true then
+                    obj.Highlight:Destroy()
             end
+            chams = Instance.new("Highlight", obj)
         end)
     end
-    for _,obj in pairs(game.Workspace.mainGame["active_anomaly"]:GetChildren()) do
-        chams = Instance.new("Highlight", obj)
-    end
-    wait(10)
 end)
