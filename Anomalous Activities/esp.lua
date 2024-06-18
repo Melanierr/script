0eac9bd23890e230a94e95e84a5661496c83c2f4
Melanie <97372099+Melanierr@users.noreply.github.com>
2024-06-18 13:37:38 +0700

    Update esp.lua

--- Anomalous Activities/esp.lua
+++ Anomalous Activities/esp.lua
@@ -1,9 +1,6 @@
 game:GetService('RunService').RenderStepped:Connect(function()
     for _,obj in pairs(game.Workspace.mainGame["active_anomaly"]:GetChildren()) do
         pcall(function()
-            if obj:FindFirstChildWhichIsA("Highlight") == true then
-                    obj.Highlight:Destroy()
-            end
             chams = Instance.new("Highlight", obj)
         end)
     end
