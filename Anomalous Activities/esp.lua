911c2e73da075a46315168ad5950656ac5a5981d
Melanie <97372099+Melanierr@users.noreply.github.com>
2024-06-21 09:17:47 +0700

    new better method

--- Anomalous Activities/esp.lua
+++ Anomalous Activities/esp.lua
@@ -1,7 +1,21 @@
-game:GetService('RunService').RenderStepped:Connect(function()
-    for _,obj in pairs(game.Workspace.mainGame["active_anomaly"]:GetChildren()) do
-        pcall(function()
-            chams = Instance.new("Highlight", obj)
-        end)
-    end
+function createMarker(obj)
+    bbg = Instance.new("BillboardGui", obj)
+    bbg.AlwaysOnTop = true
+    bbg.MaxDistance = math.huge
+    bbg.LightInfluence = 1
+    bbg.StudsOffset = Vector3.new(0, 3, 0)
+    bbg.Size = UDim2.new(0, 30, 0, 30)
+    tl = Instance.new("TextLabel", bbg)
+    tl.Font = "SciFi"
+    tl.Text = "▼"
+    tl.TextColor3 = Color3.new(240,128,128)
+    tl.BackgroundTransparency = 1
+    tl.TextScaled = true
+    tl.Size = UDim2.new(1, 0, 1, 0)  
+end
+spawn(function()
+    game:GetService("Workspace").mainGame.active_anomaly.ChildAdded:Connect(function(sub)
+        createMarker(sub)
+    end)
 end)
+print("executed")
