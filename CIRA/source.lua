1fc1ecb9f5122fb0a5a81e78403eb078e89e8126
Melanie <97372099+Melanierr@users.noreply.github.com>
2024-06-29 15:46:21 +0700

    Update source.lua

--- CIRA/source.lua
+++ CIRA/source.lua
@@ -3,27 +3,17 @@ function createMarker(obj)
     bbg.AlwaysOnTop = true
     bbg.MaxDistance = math.huge
     bbg.LightInfluence = 1
-    bbg.StudsOffset = Vector3.new(0, 3, 0)
+    bbg.StudsOffset = Vector3.new(0, 5, 0)
     bbg.Size = UDim2.new(0, 30, 0, 30)
     tl = Instance.new("TextLabel", bbg)
     tl.Font = "SciFi"
-    tl.Text = "[]"
+    tl.Text = "💩"
     tl.TextColor3 = Color3.new(240,128,128)
     tl.BackgroundTransparency = 1
     tl.TextScaled = true
-    tl.Size = UDim2.new(1, 0, 1, 0)  
+    tl.Size = UDim2.new(1, 0, 1, 0)
 end
-function setData(obj)
-    obj:SetAttribute("Swingrate", 0.001)
-    obj:SetAttribute("LungeRate", 0.001)
-    obj:SetAttribute("Firerate", 0.001)
-    obj:SetAttribute("Throwrate", 0.001)
-    obj:SetAttribute("OffhandSwingRate", 0.01)
-    obj:SetAttribute("ChargeTime", 0.01)
-    obj:SetAttribute("Cooldown", 0.01)
-    obj:SetAttribute("ThrowCooldown", 0.01)
-    obj:SetAttribute("KunaiCooldown", 0.1)
-end
+
 local char = game.Players.LocalPlayer.Character
 local bp = game.Players.LocalPlayer.Backpack
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
@@ -51,15 +41,43 @@ Section:NewButton("Parry hitbox", "skill issue?", function()
 end)
 Section:NewButton("Modify weapon", "", function()
     pcall(function()
-        for _,a in pairs(bp:GetChildren()) do
-            setData(a)
+        for _,obj in pairs(bp:GetChildren()) do
+            obj:SetAttribute("Swingrate", 0.001)
+            obj:SetAttribute("LungeRate", 0.001)
+            obj:SetAttribute("Firerate", 0.001)
+            obj:SetAttribute("Throwrate", 0.001)
+            obj:SetAttribute("OffhandSwingRate", 0.01)
+            obj:SetAttribute("ChargeTime", 0.01)
+            obj:SetAttribute("Cooldown", 0.01)
+            obj:SetAttribute("ThrowCooldown", 0.01)
+            obj:SetAttribute("KunaiCooldown", 0.1)
         end
-        for _,b in pairs(char:GetChildren()) do
-            if b:IsA("Tool") then 
-                setData(b) 
+        for _,objs in pairs(char:GetChildren()) do
+            if objs:IsA("Tool") then
+                objs:SetAttribute("Swingrate", 0.001)
+                objs:SetAttribute("LungeRate", 0.001)
+                objs:SetAttribute("Firerate", 0.001)
+                objs:SetAttribute("Throwrate", 0.001)
+                objs:SetAttribute("OffhandSwingRate", 0.01)
+                objs:SetAttribute("ChargeTime", 0.01)
+                objs:SetAttribute("Cooldown", 0.01)
+                objs:SetAttribute("ThrowCooldown", 0.01)
+                objs:SetAttribute("KunaiCooldown", 0.1)
             end
         end
     end)
+end)
+Section:NewButton("Loop TP [ use gun ], stop when new waves", "", function()
+    while true do
+        pcall(function()
+            for i,v in pairs(game.Workspace.Enemies:GetChildren()) do
+                if v.Name ~= "Cannon" then
+                    v.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.Torso.CFrame + Vector3.new(10, 3, 0)
+                end
+            end
+        end)
+        wait()
+    end
 end)
 local Tab = Window:NewTab("Visual")
 local Section = Tab:NewSection("Enemy")
@@ -74,7 +92,7 @@ local Tab = Window:NewTab("Player")
 local Section = Tab:NewSection("")
 Section:NewButton("Flyjump", "", function()
     game:GetService("UserInputService").JumpRequest:Connect(function()
-		game.Players.LocalPlayer.Character:FindFirstChildWhichIsA("Humanoid"):ChangeState(Enum.HumanoidStateType.Jumping)
+        game.Players.LocalPlayer.Character:FindFirstChildWhichIsA("Humanoid"):ChangeState(Enum.HumanoidStateType.Jumping)
     end)
 end)
 local Tab = Window:NewTab("Misc")
