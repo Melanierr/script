23060882d98a89e067342a3c6f046703e4c9786e
Melanie <97372099+Melanierr@users.noreply.github.com>
2024-08-13 10:08:06 +0700

    Update and rename Derelict.lua to derelict/source.lua

--- derelict/source.lua
+++ derelict/source.lua
@@ -8,11 +8,13 @@ local interactable = instance.Interactables
 local veggies = {"BrownMushroom", "Onion", "Rosemary", "Grain", "Potato", "Basil", "Lettuce", "Garlic", "Powerwort", "Soul Shroom", "Translucent Petal", "Fennel", "Nightshade"}
 local enemies = {}
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Dark Soul ah game", "Synapse")
+local Window = Library.CreateLib("Dark Soul", "Synapse")
 local main = Window:NewTab("Main")
 local visual = Window:NewTab("Visual")
+local player = Window:NewTab("Player")
 local msection = main:NewSection("")
 local vsection = visual:NewSection("")
+local psection = player:NewSection("")
 for _,id in pairs(hitbox:GetChildren()) do
     if tostring(id.P.Value) == plr.Name then
         pid = id
@@ -47,7 +49,10 @@ function createBG(part, name)
     c.BackgroundTransparency = 1
     c.TextScaled = false
     c.Size = UDim2.new(1.5, 0, 1.5, 0)  
-end	
+end
+local function changeWalkspeed(speed)
+	plr.Character.Humanoid.WalkSpeed = speed
+end
 -- // 
 msection:NewToggle("Bonsai", "", function(state)
     if state then
@@ -61,16 +66,11 @@ msection:NewButton("Anti Combat Log", "", function()
     	pid.HumanoidAttributes:SetAttribute("InCombat", false)
 	end)
 end)
-msection:NewButton("Anti Ragdoll", "", function()
-	game:GetService('RunService').RenderStepped:connect(function()
-    	pid.HumanoidAttributes:SetAttribute("IsRagdoll", false)
-	end)
-end)
 msection:NewSlider("Attack Speed", "", 9, 1, function(atkspeed)
     stats:SetAttribute("AttackSpeed", atkspeed)
 end)
-msection:NewSlider("Agility [ In Combat ]", "", 500, 200, function(speed)
-    stats:SetAttribute("Agility", speed)
+msection:NewSlider("Agility [ In Combat ]", "", 500, 200, function(agl)
+    stats:SetAttribute("Agility", agl)
 end)
 vsection:NewButton("Find chest", "", function()
     for _,chest in pairs(map.Objects:GetChildren()) do
@@ -93,17 +93,20 @@ vsection:NewButton("Forage ESP", "ButtonInfo", function()
 	end
 end)
 vsection:NewButton("Clear all esp", "", function()
-	for _,esp in pairs(instance:GetDescendants()) do
+	for _,esp in pairs(map.Objects:GetDescendants()) do
 		if esp:IsA("BillboardGui") or esp:IsA("BoxHandleAdornment") then
 			esp:Destroy()
 		end
 	end
-	for _,espp in pairs(map.Objects:GetDescendants()) do
-		if espp:IsA("BillboardGui") or espp:IsA("BoxHandleAdornment") then
-			espp:Destroy()
+	for _,esp in pairs(interactable:GetDescendants()) do
+		if esp:IsA("BillboardGui") or esp:IsA("BoxHandleAdornment") then
+			esp:Destroy()
 		end
 	end
- end)
+end)
 vsection:NewButton("Remove loading screen", "", function()
    plr.PlayerGui.MainGui.Loading.Visible = false
+end)
+psection:NewSlider("Walkspeed", "", 500, 30, function(walkspeed)
+    plr.Character.Humanoid:GetPropertyChangedSignal("WalkSpeed"):Connect(changeWalkspeed(walkspeed))
 end)
