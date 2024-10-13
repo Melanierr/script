944d5fc700ef06298f78a673a9b6ade0e3d146c3
Melanie <97372099+Melanierr@users.noreply.github.com>
2024-10-13 14:53:27 +0700

    hitbox cheat now remove helmet

--- gravedigga.lua
+++ gravedigga.lua
@@ -8,15 +8,16 @@ Section:NewButton("Remove camera fog", "ButtonInfo", function()
 end)
 Section:NewButton("Hitbox Extender", "ButtonInfo", function()
     game:GetService('RunService').RenderStepped:connect(function()
-            for i,v in next, game:GetService('Players'):GetPlayers() do
-                if v.Name ~= game:GetService("Players").LocalPlayer.Name and v.Team ~= game:GetService("Players").LocalPlayer.Team then
-                    pcall(function()
-                        local hl = Instance.new("Highlight", v)
-                        v.Character.HeadHitbox.Material = "Plastic"
-                        v.Character.HeadHitbox.Transparency = 0.5
-                        v.Character.HeadHitbox.Size = Vector3.new(4,4,4)
-                    end)
-                end
+        for _,plr in next, game:GetService('Players'):GetPlayers() do
+            if plr.Name ~= game:GetService("Players").LocalPlayer.Name and plr.Team ~= game:GetService("Players").LocalPlayer.Team then
+                pcall(function()
+                    local hl = Instance.new("Highlight", v)
+                    plr.Character:FindFirstChild("helmet"):Destroy()
+                    plr.Character.HeadHitbox.Material = "Plastic"
+                    plr.Character.HeadHitbox.Transparency = 0.5
+                    plr.Character.HeadHitbox.Size = Vector3.new(4,4,4)
+                end)
             end
+        end
     end)
 end)
