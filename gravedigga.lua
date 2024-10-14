eb21d4a8a52f2e3d5a43acfb0e72af97e0f5493c
Melanie <97372099+Melanierr@users.noreply.github.com>
2024-10-14 23:07:30 +0700

    Update gravedigga.lua

--- gravedigga.lua
+++ gravedigga.lua
@@ -4,16 +4,6 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHept
 local Window = Library.CreateLib("Grave Digger", "Synapse")
 local Tab = Window:NewTab("Main")
 local Section = Tab:NewSection("")
---// function
-function wallhack(obj)
-    local wh = Instance.new("BoxHandleAdornment")
-    wh.Parent = obj
-    wh.Adornee = obj
-    wh.AlwaysOnTop = true
-    wh.ZIndex = 0
-    wh.Size = obj.Size
-    wh.Transparency = 0.8
-end
 --// buttons
 Section:NewButton("Friendly Fire", "", function()
     plr.LocalPlayer.Character.Parent = game.Workspace
@@ -23,24 +13,14 @@ Section:NewButton("Hitbox Expander", "", function()
         for _,plrs in next, game:GetService('Players'):GetPlayers() do
             if plrs.Team ~= plr.LocalPlayer.Team then
                 pcall(function()
-                    plrs.Character.HeadHitbox.Transparency = 0.8
-                    plrs.Character.HeadHitbox.Size = Vector3.new(4,4,4)
+                    plrs.Character.HeadHitbox.Transparency = 0.9
+                    plrs.Character.HeadHitbox.Size = Vector3.new(4.5,4.5,4.5)
+                    plrs.Character.HeadHitbox.CanCollide = false
                 end)
             end
         end
     end)
 end)
 Section:NewButton("Wallhack", "", function()
-    rs.RenderStepped:Connect(function()
-        for _,hw in pairs(plr:GetPlayers()) do
-            if hw.Name ~= plr.LocalPlayer.Name and hw.Team ~= plr.LocalPlayer.Team and hw.Character:FindFirstChild("Humanoid").Health ~= 0 then
-                pcall(function()
-                    if hw.Character.Head:FindFirstChildWhichIsA("BoxHandleAdornment") == false and not hw.Character.Torso:FindFirstChildWhichIsA("BoxHandleAdornment") then
-                        wallhack(hw.Character:FindFirstChild("Head"))
-                        wallhack(hw.Character:FindFirstChild("Torso"))
-                    end
-                end)
-            end
-        end
-    end)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Melanierr/script/refs/heads/main/Misc/synapsesp.lua"))()
 end)
