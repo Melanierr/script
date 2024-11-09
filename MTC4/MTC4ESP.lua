6627d3fafe913cabcc021d16b36999ffae8c7dd8
Melanie <97372099+Melanierr@users.noreply.github.com>
2024-11-10 01:27:29 +0700

    Update MTC4ESP.lua

--- MTC4/MTC4ESP.lua
+++ MTC4/MTC4ESP.lua
@@ -10,9 +10,6 @@ local esp = game:GetService("RunService").RenderStepped:Connect(function()
     for _,v in pairs(plr:GetPlayers()) do
         if v.Team ~= plr.LocalPlayer.Team and not v.Character:FindFirstChildOfClass("Highlight") then
             box(v.Character)
-            v.Character.Humanoid.Died:Connect(function()
-                esp:Disconnect()
-            end)
         end
     end
 end)
