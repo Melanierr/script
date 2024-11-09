f318b424cbe2bcad61aaaafb6257ff8fec1f88f9
Melanie <97372099+Melanierr@users.noreply.github.com>
2024-11-10 01:36:12 +0700

    Update MTC4RF.lua

--- MTC4/MTC4RF.lua
+++ MTC4/MTC4RF.lua
@@ -1,13 +1,12 @@
 local Player = game.Players.LocalPlayer
 local rf = Instance.new("ScreenGui")
+local distances = Instance.new("TextLabel")
 rf.Name = "RangefinderGUI"
 rf.Parent = Player:WaitForChild("PlayerGui")
 rf.DisplayOrder = 20
-
-local distances = Instance.new("TextLabel")
 distances.Name = "distances"
 distances.Size = UDim2.new(0, 200, 0, 50)
-distances.Position = UDim2.new(0.5, -100, 0, 50) 
+distances.Position = UDim2.new(0.5, -100, 0, 50)
 distances.BackgroundTransparency = 1
 distances.TextScaled = true
 distances.TextColor3 = Color3.new(1, 1, 1) 
@@ -19,7 +18,7 @@ local function updateDistance()
     if mouse.Target then
         local tpos = mouse.Hit.Position
         local ppos = Player.Character.HumanoidRootPart.Position
-        local ds = math.ceil(ppos - tpos).Magnitude * 0.28 + 30)
+        local ds = math.ceil((ppos - tpos).Magnitude * 0.28 + 30)
         distances.Text = string.format("Distance: %d meters", ds)
     else
         distances.Text = "Out of Range"
