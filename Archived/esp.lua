e324f99d09efc6a39d5b0301c850a0f4d34ce5ef
Musc <97372099+Melanierr@users.noreply.github.com>
2026-01-01 20:15:33 +0700

    Overally improved the ESP

--- Archived/esp.lua
+++ Archived/esp.lua
@@ -1,31 +1,45 @@
-local plr = game.Players.LocalPlayer.Character
-local npc = game:GetService("Workspace").NPCS
 local mob = game:GetService("Workspace").Alive
-local map = workspace.Map
-local stuff = game:GetService("Workspace").Thrown
+local plr = game:GetService("Players").LocalPlayer
+
 local function ESP(part)
-    local humanoid = part:FindFirstChildOfClass("Humanoid")
-    if not humanoid or part:FindFirstChildOfClass("BillboardGui") then return end
-
-    local gui = Instance.new("BillboardGui", part)
-    gui.Name, gui.StudsOffset, gui.MaxDistance, gui.Adornee, gui.LightInfluence, gui.AlwaysOnTop, gui.Size ="ESP", Vector3.new(0, 4, 0), 250, part, 1, true, UDim2.new(0, 100, 0, 50)
-
-    local label = Instance.new("TextLabel", gui)
-    label.Name, label.TextSize, label.BackgroundTransparency, label.TextColor3, label.Size,
-    label.Position, label.AnchorPoint, label.TextStrokeTransparency = "HP", 12, 1, Color3.fromRGB(255, 255, 0), UDim2.new(1, 0, 0, 15), UDim2.new(0.5, 0, 0, 0), Vector2.new(0.5, 0), 0.7
-
-    local function update() label.Text = part.Name .. " Health: " .. math.floor(humanoid.Health) end
-    update()
-    humanoid:GetPropertyChangedSignal("Health"):Connect(update)
+    if part:FindFirstChildOfClass("BillboardGui") then return end
+    if not part:FindFirstChildOfClass("Humanoid") then return end
+    local a = Instance.new("BillboardGui")
+    local b = Instance.new("TextLabel")
+    local hp = part:FindFirstChildOfClass("Humanoid")
+    local health = math.floor(hp.Health)
+    a.Parent = part
+    b.Parent = a
+    a.Name = "SKIBIDI"
+    a.StudsOffset = Vector3.new(0, 4, 0)
+    a.MaxDistance = 200
+    a.Adornee = part
+    a.LightInfluence = 1
+    a.AlwaysOnTop = true
+    a.Size = UDim2.new(0, 100, 0, 50)
+    b.Name = "HP"
+    b.TextSize = 12
+    b.BackgroundTransparency = 1
+    b.TextColor3 = Color3.fromRGB(255, 255, 0)
+    b.Size = UDim2.new(1, 0, 0, 15)
+    b.Position = UDim2.new(0.5, 0, 0, 0) 
+    b.AnchorPoint = Vector2.new(0.5, 0)
+    b.TextStrokeTransparency = 0.7
+    b.Text = part.Name .. " Health: " .. health
+    hp:GetPropertyChangedSignal("Health"):Connect(function()
+        health = math.floor(hp.Health)
+        b.Text = part.Name .. " Health: " .. health
+    end)
 end
 game:GetService("UserInputService").InputBegan:Connect(function(input)
     if input.KeyCode == Enum.KeyCode.K then
-      for _, v in pairs(mob:GetChildren()) do
-            if v:IsA("Model") and v.Name ~= plr.Name then
+        for _, v in pairs(mob:GetChildren()) do
+            if v:IsA("Model") and v ~= plr.Character then
                 if v then
                     ESP(v)
                 end
             end
         end
+        print("ran")
     end
 end)
