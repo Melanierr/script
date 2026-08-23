a114f68919a11ab16ec5b834f9665be8a0605f3a
Musc <97372099+Melanierr@users.noreply.github.com>
2026-08-23 12:51:58 +0700

    Update betteresp.lua

--- Red/GraveDigger/betteresp.lua
+++ Red/GraveDigger/betteresp.lua
@@ -1,43 +1,40 @@
-local uis = game:GetService("UserInputService")
-local plr = game:GetService("Players")
-local lplr = plr.LocalPlayer
-
+local uis = game:GetService("UserInputService");
+local plr = game:GetService("Players");
+local lplr = plr.LocalPlayer;
 local function esp(obj)
-    local esp = Instance.new("BoxHandleAdornment", obj)
-    esp.Adornee = obj
-    esp.Name = "##ss"
-    esp.AlwaysOnTop = true
-    esp.Color3 = _G.espColor
-    esp.Transparency = 0.5
-    esp.ZIndex = 0
-    esp.Size = Vector3.new(1, 1, 1)
-    warn("oo put marker on this " .. obj.Name)
+	local esp = Instance.new("BoxHandleAdornment", obj);
+	esp.Adornee = obj;
+	esp.Name = "##ss";
+	esp.AlwaysOnTop = true;
+	esp.Color3 = _G.espColor;
+	esp.Transparency = 0.5;
+	esp.ZIndex = 0;
+	esp.Size = Vector3.new(1, 1, 1);
+	warn("oo put marker on this " .. obj.Name);
 end
-
 local function delete()
-    for i,v in pairs(game:GetService("Workspace"):GetDescendants()) do
-        if v:IsA("BoxHandleAdornment") and v.Parent.Parent.Humanoid.Health == 0 then
-            v:Destroy()
-            wait()
-            warn("deleted")
-        end
-    end
+	for i, v in pairs(game:GetService("Workspace"):GetDescendants()) do
+		if (v:IsA("BoxHandleAdornment") and (v.Parent.Parent.Humanoid.Health == 0)) then
+			v:Destroy();
+			wait();
+			warn("deleted");
+		end
+	end
 end
-
 uis.InputBegan:Connect(function(input, gameProcessed)
-    if input.KeyCode == Enum.KeyCode[_G.keyBind] then
-        delete()
-        local plrteam = lplr.Character.Parent
-        for _,hitbox in pairs(game.Workspace:GetDescendants()) do
-            if hitbox.Name == "HeadHitbox" and hitbox.Parent.Parent.Name ~=  plrteam.Name then
-                local humanoid = hitbox.Parent.Humanoid
-                if humanoid and humanoid.Health > 0 and not hitbox:FindFirstChild("##ss") then
-                    hitbox.Size = _G.headSize
-                    hitbox.Transparency = 0.5
-                    esp(hitbox)    
-                end
-            end
-        end
-    end
-end)
-print("Script is loaded")
+	if (input.KeyCode == Enum.KeyCode[_G.keyBind]) then
+		delete();
+		local plrteam = lplr.Character.Parent;
+		for _, hitbox in pairs(game.Workspace:GetDescendants()) do
+			if ((hitbox.Name == "HeadHitbox") and (hitbox.Parent.Parent.Name ~= plrteam.Name)) then
+				local humanoid = hitbox.Parent.Humanoid;
+				if (humanoid and (humanoid.Health > 0) and not hitbox:FindFirstChild("##ss")) then
+					hitbox.Size = _G.headSize;
+					hitbox.Transparency = 0.5;
+					esp(hitbox);
+				end
+			end
+		end
+	end
+end);
+print("Script is loaded");
